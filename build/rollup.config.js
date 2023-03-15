98c136c7618580a79ec02becf15a6afe9b3cd14e
xiaohuibuhuifei <391078083@qq.com>
2023-03-15 16:04:34 +0800

    rollup配置修改

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -5,8 +5,8 @@ import json from "@rollup/plugin-json";
 // terser 代码压缩
 import terser from "@rollup/plugin-terser";
 
-// var outputPath = "dist/";
-var outputPath = "D:/i-workspace/vue3-cesium/public/twearth/";
+var outputPath = "dist/";
+// var outputPath = "D:/i-workspace/vue3-cesium/public/twearth/";
 
 // 通用包配置
 export default {
